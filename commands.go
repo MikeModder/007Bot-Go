4fcfc84d9f25358119a97ea970274d4a51a3e889
MikeModder <oddman56@protonmail.com>
2019-01-13 09:46:23 -0500

    update bot for new version of anpan

--- commands.go
+++ commands.go
@@ -5,33 +5,34 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/MikeModder/anpan"
 	"github.com/bwmarrin/discordgo"
 )
 
 func registerCommands() {
-	handler.AddCommand("about", "Show information about the bot", false, 0, aboutCmd)
-	handler.AddCommand("ping", "Check the bot's ping", false, 0, pingCmd)
-	handler.AddCommand("version", "Show the versions of things", false, 0, versionCmd)
+	handler.AddCommand("about", "Show information about the bot", false, false, 0, aboutCmd)
+	handler.AddCommand("ping", "Check the bot's ping", false, false, 0, pingCmd)
+	handler.AddCommand("version", "Show the versions of things", false, false, 0, versionCmd)
 
 	// Add the default help command
 	handler.AddDefaultHelpCommand()
 }
 
-func aboutCmd(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) {
+func aboutCmd(ctx anpan.Context, _ []string) {
 	embed := &discordgo.MessageEmbed{
 		Title:       "About 007Bot (Golang edition):",
 		Description: "Hello, I am [007Bot Golang Edition](https://github.com/MikeModder/007Bot-Go)! I am a rewrite of the original 007Bot in [Golang](https://golang.org) using the [anpan](https://github.com/MikeModder/anpan) command handler!",
 		Footer: &discordgo.MessageEmbedFooter{
 			Text:    "Wow, look at me!",
-			IconURL: s.State.User.AvatarURL("512"),
+			IconURL: ctx.Session.State.User.AvatarURL("512"),
 		},
 	}
 
-	s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, embed)
 }
 
-func pingCmd(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) {
-	ping, _ := s.ChannelMessageSend(m.ChannelID, "Let me check that for you!")
+func pingCmd(ctx anpan.Context, _ []string) {
+	ping, _ := ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, "Let me check that for you!")
 
 	tsOne, _ := ping.Timestamp.Parse()
 	took := time.Now().Sub(tsOne)
@@ -41,14 +42,14 @@ func pingCmd(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) {
 		Description: fmt.Sprintf("Ping took `%s`!", took.String()),
 	}
 
-	s.ChannelMessageEditEmbed(m.ChannelID, ping.ID, embed)
+	ctx.Session.ChannelMessageEditEmbed(ctx.Message.ChannelID, ping.ID, embed)
 }
 
-func versionCmd(s *discordgo.Session, m *discordgo.MessageCreate, _ []string) {
+func versionCmd(ctx anpan.Context, _ []string) {
 	embed := &discordgo.MessageEmbed{
 		Title:       "007Bot Version(s)",
 		Description: fmt.Sprintf("Golang: %s", runtime.Version()),
 	}
 
-	s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, embed)
 }
